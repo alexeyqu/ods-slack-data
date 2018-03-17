3a929009926bc0fa06e748d88bc410c312cbe7db
Alexey Kulikov <alexeyqu@gmail.com>
2018-03-17 17:46:15 +0300

    users parser

--- src/users_parser.py
+++ src/users_parser.py
@@ -10,18 +10,19 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.pool import NullPool
 
 logger = logging.getLogger(__name__)
-db_name = '../ods-slack.db'
-engine = create_engine('sqlite:///' + db_name, echo=True)
+local_name = 'sqlite:///../ods-slack-user.db'
+remote_name = 'postgres://usgbqmayetwlrv:a8b6a60b922bd6d08c3e94fa41eac937f71ed3bc4afade4995a3bdf5d54e36ca@ec2-54-247-81-88.eu-west-1.compute.amazonaws.com:5432/d7942vtj104cpv'
+engine = create_engine(local_name, echo=True)
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
 
-class User(Base):
-    __tablename__ = 'user'
+class UserData(Base):
+    __tablename__ = 'imported_user_data'
 
     id = Column('id', String, primary_key=True)
     name = Column('name', String)
     deleted = Column('deleted', Boolean)
-    real_name = Column('real_name', String)
+
     tz = Column('tz', String)
     tz_label = Column('tz_label', String)
     tz_offset = Column('tz_offset', Integer)
@@ -33,32 +34,6 @@ class User(Base):
     is_bot = Column('is_bot', Boolean)
     updated = Column('updated', Integer)
     is_app_user = Column('is_app_user', Boolean)
-
-    def __init__(self, data):
-        self.id = data.get('id')    
-        self.name = data.get('name')
-        self.deleted = data.get('deleted', False)
-        self.real_name = data.get('real_name', '')
-        self.tz = data.get('tz', '')
-        self.tz_label = data.get('tz_label', '')
-        self.tz_offset = data.get('tz_offset', 0)
-        self.is_admin = data.get('is_admin', False)
-        self.is_owner = data.get('is_owner', False)
-        self.is_primary_owner = data.get('is_primary_owner', False)
-        self.is_restricted = data.get('is_restricted', False)
-        self.is_ultra_restricted = data.get('is_ultra_restricted', False)
-        self.is_bot = data.get('is_bot', False)
-        self.updated = data.get('updated', 0)
-        self.is_app_user = data.get('is_app_user', False)
-
-    def __repr__(self):
-        return "<User({0}, {1})>".format(self.id, self.name)
-
-
-class UserData(Base):
-    __tablename__ = 'user_data'
-
-    id = Column('id', String, ForeignKey("user.id"), primary_key=True)
 
     title = Column('title', String)                               
     phone = Column('phone', String)                               
@@ -81,28 +56,43 @@ class UserData(Base):
     image_512 = Column('image_512', String)                               
     team = Column('team', String)        
 
-    def __init__(self, id, data):
-        self.id = id
-        self.title = data.get('title', '')
-        self.phone = data.get('phone', '')
-        self.skype = data.get('skype', '')
-        self.real_name = data.get('real_name', '')
-        self.real_name_normalized = data.get('real_name_normalized', '')
-        self.display_name = data.get('display_name', '')
-        self.display_name_normalized = data.get('display_name_normalized', '')
+    def __init__(self, data):
+        self.id = data.get('id')
+        self.name = data.get('name')
+        self.deleted = data.get('deleted', False)
+
+        self.tz = data.get('tz', '')
+        self.tz_label = data.get('tz_label', '')
+        self.tz_offset = data.get('tz_offset', 0)
+        self.is_admin = data.get('is_admin', False)
+        self.is_owner = data.get('is_owner', False)
+        self.is_primary_owner = data.get('is_primary_owner', False)
+        self.is_restricted = data.get('is_restricted', False)
+        self.is_ultra_restricted = data.get('is_ultra_restricted', False)
+        self.is_bot = data.get('is_bot', False)
+        self.updated = data.get('updated', 0)
+        self.is_app_user = data.get('is_app_user', False)
+
+        self.title = data.get('profile', {}).get('title', '')
+        self.phone = data.get('profile', {}).get('phone', '')
+        self.skype = data.get('profile', {}).get('skype', '')
+        self.real_name = data.get('profile', {}).get('real_name', '')
+        self.real_name_normalized = data.get('profile', {}).get('real_name_normalized', '')
+        self.display_name = data.get('profile', {}).get('display_name', '')
+        self.display_name_normalized = data.get('profile', {}).get('display_name_normalized', '')
         # self.fields = data.get('fields', '')
-        self.status_text = data.get('status_text', '')
-        self.status_emoji = data.get('status_emoji', '')
-        self.avatar_hash = data.get('avatar_hash', '')
-        self.first_name = data.get('first_name', '')
-        self.last_name = data.get('last_name', '')
-        self.image_24 = data.get('image_24', '')
-        self.image_32 = data.get('image_32', '')
-        self.image_48 = data.get('image_48', '')
-        self.image_72 = data.get('image_72', '')
-        self.image_192 = data.get('image_192', '')
-        self.image_512 = data.get('image_512', '')
-        self.team = data.get('team', '')
+        self.status_text = data.get('profile', {}).get('status_text', '')
+        self.status_emoji = data.get('profile', {}).get('status_emoji', '')
+        self.avatar_hash = data.get('profile', {}).get('avatar_hash', '')
+        self.first_name = data.get('profile', {}).get('first_name', '')
+        self.last_name = data.get('profile', {}).get('last_name', '')
+        self.image_24 = data.get('profile', {}).get('image_24', '')
+        self.image_32 = data.get('profile', {}).get('image_32', '')
+        self.image_48 = data.get('profile', {}).get('image_48', '')
+        self.image_72 = data.get('profile', {}).get('image_72', '')
+        self.image_192 = data.get('profile', {}).get('image_192', '')
+        self.image_512 = data.get('profile', {}).get('image_512', '')
+        self.team = data.get('profile', {}).get('team', '')
 
     def __repr__(self):
         return "<UserData({0}, {1})>".format(self.id, self.real_name)
@@ -112,12 +102,11 @@ def parse_users(json_path):
     session = Session()
     data = json.load(open(json_path))
     for user in data:
-        session.add(User(user))
-        session.add(UserData(user['id'], user['profile']))
+        session.add(UserData(user))
     session.commit()
-
 
 
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
+    # UserData.__table__.drop(engine)
     parse_users('../data/users.json')
